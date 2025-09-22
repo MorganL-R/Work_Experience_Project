6fc63f09593749ee7465b9045b4ce7c79c8a5800
Kuba-original <kuba.a.jablonowski@capgemini.com>
2025-09-22 16:09:42 +0100

    backend progress

--- src/main/java/Controllers/FormController.java
+++ src/main/java/Controllers/FormController.java
@@ -1,37 +1,36 @@
-package Controllers;
-
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
 
 //this annotation denotes it as a spring boot controller, allowing it to interact with the application
 @Controller
 public class FormController {
 
-    private final HttpServletResponse httpServletResponse;
-
-    public FormController(HttpServletResponse httpServletResponse) {
-        this.httpServletResponse = httpServletResponse;
-    }
-
     //This is an example of the part you use to map code to a html page
     @GetMapping(value = {"/submitForm"})
-    protected String defaultRoute(HttpMethod httpMethod) throws Exception {
-        //code here, the return value tells the program which page to go to next
+    protected String processForm(Model model,
+                                 @RequestParam String name,
+                                 @RequestParam Integer phoneNumber,
+                                 @RequestParam LocalDate dob) throws Exception {
+
+        String Name = name.length() >= 4 ? name.substring(0, 4) : name;
+
+        String Year = String.valueOf(dob.getYear()).substring(2);
+
+        String username = Name + Year;
+
+        model.addAttribute("name", name);
+        model.addAttribute("phoneNumber", phoneNumber);
+        model.addAttribute("dob", dob);
+        model.addAttribute("username", username); // Add the new username
+
         return "submitForm";
     }
 
-    public void exceptionHandling(String ) throws Exception {
 
-    }
 }
 
-
-
-
-
-
-
+}
