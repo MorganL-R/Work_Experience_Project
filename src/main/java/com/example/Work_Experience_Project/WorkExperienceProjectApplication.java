c6bc9bdf330a5ec1885a1d9d697a589018dca098
Morgan Lawley-Robinson <morgan.a.lawley-robinson@capgemini.com>
2025-09-22 12:32:15 +0100

    Initial setup commit

---
+++ src/main/java/com/example/Work_Experience_Project/WorkExperienceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Work_Experience_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkExperienceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkExperienceProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Work_Experience_Project/WorkExperienceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Work_Experience_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkExperienceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
