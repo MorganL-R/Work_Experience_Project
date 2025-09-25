7b6f9c84a74fedcaeb954ce33fa4c026a25eac57
Ryan C <ryan.a.clapperton@capgemini.com>
2025-09-25 11:01:06 +0100

    Small changes to form validation

--- src/test/java/com/example/Work_Experience_Project/WorkExperienceProjectApplicationTests.java
+++ src/test/java/com/example/Work_Experience_Project/WorkExperienceProjectApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.Work_Experience_Project;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+import com.example.Work_Experience_Project.controllers.FormController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(FormController.class)
 class WorkExperienceProjectApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void test() throws Exception {
+        mockMvc.perform(post("/submitForm")
+                    .param("name", "Ryan")
+                    .param("phoneNumber", "1234567890")
+                    .param("dob", "2000-01-01"))
+                .andExpect(status().is(200));
 	}
 
 }
