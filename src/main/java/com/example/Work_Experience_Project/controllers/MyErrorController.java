d156f3de8da933a8eb9aaf90ae89c66be83d1e18
Morgan Lawley-Robinson <morgan.a.lawley-robinson@capgemini.com>
2026-01-19 09:52:51 +0000

    removed accidental pushes to main branch so it is back to a start-ready state

--- src/main/java/com/example/Work_Experience_Project/controllers/MyErrorController.java
+++ src/main/java/com/example/Work_Experience_Project/controllers/MyErrorController.java
@@ -1,30 +1,9 @@
 package com.example.Work_Experience_Project.controllers;
 
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import java.time.LocalDate;
-import org.springframework.web.server.ResponseStatusException;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyErrorController implements ErrorController {
-
-    @RequestMapping("/error")
-    public String handleError(Model model, HttpServletRequest request) {
-        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-        Object message = request.getAttribute(RequestDispatcher.ERROR_MESSAGE);
-
-        model.addAttribute("status", status);
-        model.addAttribute("message", message);
-
-        return "/errorPage";
-    }
 }
