f07c883de5b58714c52c999c0da0d50bec08b91d
MorganL-R <morganlawleyrobin@hotmail.com>
2026-09-23 14:22:17 +0100

    fixed test issues

--- src/test/java/com/example/Work_Experience_Project/WorkExperienceProjectApplicationTests.java
+++ src/test/java/com/example/Work_Experience_Project/WorkExperienceProjectApplicationTests.java
@@ -4,12 +4,15 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.example.Work_Experience_Project.controllers.FormController;
+import com.example.Work_Experience_Project.services.FormService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(FormController.class)
+@Import(FormService.class)
 class WorkExperienceProjectApplicationTests {
 
     @Autowired
@@ -20,6 +23,7 @@ class WorkExperienceProjectApplicationTests {
         mockMvc.perform(post("/submitForm")
                         .param("name", "Kuba")
                         .param("phoneNumber", "1234567890")
+                        .param("email", "kuba@example.com")
                         .param("dob", "2007-12-21"))
                 .andExpect(status().is(200));
     }
